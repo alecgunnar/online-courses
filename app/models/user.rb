55cdd8c689c5e51e11e01824837608157e07b038
gunnar94 <gunnar94@me.com>
2015-11-11 09:39:59 -0500

    Updated email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :email, :format => {:with => /^(.?)*@wmich.edu/, :message => "must be a Western Michigan University address."}
+  validates :email, :format => {:with => /\A(.?)*@wmich.edu/, :message => "must be a Western Michigan University address."}
 end
