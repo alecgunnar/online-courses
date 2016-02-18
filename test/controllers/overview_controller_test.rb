4a75c03c7da781e45caefe9de875ebc848874281
gunnar94 <gunnar94@me.com>
2016-02-18 09:26:40 -0500

    Removed old tests

--- test/controllers/overview_controller_test.rb
+++ test/controllers/overview_controller_test.rb
@@ -1,8 +1,4 @@
 require 'test_helper'
 
 class OverviewControllerTest < ActionController::TestCase
-  test 'boot without session' do
-    get :index
-    assert_redirected_to sign_in_path
-  end
 end
